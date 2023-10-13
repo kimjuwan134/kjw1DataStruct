debebcb3606dd14bee1611c4d1abf0e58ff1c1ef
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-10-13 19:47:23 +0900

    (2023.10.13) 원형 연결 리스트

--- kjwDataStruct/DobleLinkedList.h
+++ kjwDataStruct/DobleLinkedList.h
@@ -47,5 +47,104 @@ public :
 		}
 		size++;
 	}
+
+	void PushBack(T data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+		newNode->next = nullptr;
+		newNode->previous = nullptr;
+		if (head == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			newNode.
+		}
+		size++;
+	}
+
+	void PopFront()
+	{
+		if (head == nullptr)
+		{
+			cout << "List is Empty" << endl;
+			return;
+		}
+		else if (size == 1)
+		{
+			Node* deleteNode = head;
+			head = nullptr;
+			tail = nullptr;
+			delete deleteNode;
+		}
+		else
+		{
+			Node* deleteNode = head;
+			head = head->next;
+			head->previous = nullptr;
+			delete deleteNode;
+		}
+		size--;
+	}
+
+	void PopBack()
+	{
+		if (head == nullptr)
+		{
+			cout << "List is Empty" << endl;
+			return;
+		}
+		else if (size == 1)
+		{
+			Node* deleteNode = head;
+			head = nullptr;
+			tail = nullptr;
+			delete deleteNode;
+		}
+		else
+		{
+			Node* deleteNode = tail;
+			tail = tail->previous;
+			tail->next = nullptr;
+			delete deleteNode;
+		}
+		size--;
+	}
+
+	void Insert(int position, T data)
+	{
+		if (position <= 1)
+		{
+			PushFront(data);
+			return;
+		}
+
+		Node* currentNode = head;
+
+		for (int i = 1; i < position; i++)
+		{
+			currentNode = currentNode->next;
+		}
+
+		Node* newNode = new Node;
+		newNode->next = currentNode;
+		currentNode->previous->next = newNode;
+		newNode->previous = currentNode->previous;
+		currentNode->previous = newNode;
+		size++;
+	}
+
+	~DobleLinkedList()
+	{
+		while (head != nullptr)
+		{
+			Node* deleteNode = head;
+			head = head->next;
+			delete deleteNode;
+		}
+	}
 };
 
