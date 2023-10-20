b09d49d23e0848c7f5881c1fa331f920fd36c406
hlf1 <139342292+kimjuwan134@users.noreply.github.com>
2023-10-20 19:45:37 +0900

    (2023.10.20) 시간 복잡도

---
+++ kjwDataStruct/SelectSort.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+#define SIZE 5
+
+
+int main()
+{
+#pragma region 선택 정렬
+
+	// 정렬되지 않은 데이터들에 대해 가장 작은 데이터를 찾아서 
+	// 가장 앞에 있는 데이터와 교환하는 알고리즘.
+
+	// 시간복잡도 O(n^2)
+
+	int arr[SIZE] = { 5,8,3,9,1 };
+
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		int min = arr[i];
+
+		int select = i;
+
+		for (int j = i + 1; j < SIZE; j++)
+		{
+			if (min > arr[j])
+			{
+				min = arr[j];
+				select = j;
+			}
+		}
+		swap(arr[i], arr[select]);
+	}
+
+	for (const int& element : arr)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+
+
+
+	return 0;
+}
