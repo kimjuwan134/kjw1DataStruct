a8238afe6d6c09c01a5c6bfd0a5d9a586d090ddf
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-10-25 11:12:44 +0900

    1

--- kjwDataStruct/kjwDataStruct.cpp
+++ kjwDataStruct/kjwDataStruct.cpp
@@ -13,16 +13,26 @@ int main()
 
 	// 시간 복잡도 : O(n+k)
 
-	int arr[10] = { 1,3,5,4,2,4,1,3,5,1 };
-	int brr[5];
-	int count = 0;
+	int arr[10] = { 5,4,3,2,1,2,3,4,5,1 };
+	int brr[5] = { 0, };
 
 	for (int i = 0; i < 10; i++)
 	{
-		if (arr[i] == 10/(i+1)) count++;
-
+		if (arr[i] == 1) brr[0] += 1;
+		else if (arr[i] == 2) brr[1] += 1;
+		else if (arr[i] == 3) brr[2] += 1;
+		else if (arr[i] == 4) brr[3] += 1;
+		else if (arr[i] == 5) brr[4] += 1;
 	}
 
+	for (int i = 0; i < 5; i++)
+	{
+		while (brr[i])
+		{
+			cout << i + 1;
+			brr[i]--;
+		}
+	}
 
 #pragma endregion
 
